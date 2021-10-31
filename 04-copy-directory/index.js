cf75537f6efc030ef286a41a4c7345681c70317a
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2021-10-31 14:47:44 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
 
+fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true}, (err)=>{
+  if(err) throw err;
+})
+
+fs.readdir(path.join(__dirname, './files-copy'), {withFileTypes: true}, (err, files)=>{
+  if (err) {
+    console.error(err);
+    return;
+  }
+  files.forEach(file=>{
+    fs.unlink(path.join(__dirname, './files-copy', file.name), (err)=>{
+      if (err) {
+        console.error(err);
+        return;
+      }
+    })
+  })
+});
+
+fs.readdir(path.join(__dirname, './files'), {withFileTypes: true}, (err, files)=>{
+  if (err) {
+    console.error(err);
+    return;
+  }
+  files.forEach(file=>{
+    fs.copyFile(path.join(__dirname, './files', file.name), path.join(__dirname, './files-copy', file.name), (err)=>{
+      if (err) {
+        console.error(err);
+        return;
+      }
+    })
+  })
+})
