d30bbae0f57c87903d1974176f45ce1e053a269a
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2021-10-31 15:07:14 +0300

    fix: emplement copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const { stdout } = process;
 
+
+
 fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true}, (err)=>{
-  if(err) throw err;
+  if (err) {console.error(err)}
 })
 
 fs.readdir(path.join(__dirname, './files-copy'), {withFileTypes: true}, (err, files)=>{
@@ -12,26 +14,25 @@ fs.readdir(path.join(__dirname, './files-copy'), {withFileTypes: true}, (err, fi
     return;
   }
   files.forEach(file=>{
-    fs.unlink(path.join(__dirname, './files-copy', file.name), (err)=>{
-      if (err) {
-        console.error(err);
-        return;
-      }
+    fs.unlink(path.join(__dirname, './files-copy', file.name), err=>{
+      if (err) {console.error(err)}
     })
   })
+  copyDir();
 });
 
-fs.readdir(path.join(__dirname, './files'), {withFileTypes: true}, (err, files)=>{
-  if (err) {
-    console.error(err);
-    return;
-  }
-  files.forEach(file=>{
-    fs.copyFile(path.join(__dirname, './files', file.name), path.join(__dirname, './files-copy', file.name), (err)=>{
-      if (err) {
-        console.error(err);
-        return;
-      }
+function copyDir() {
+  console.log('copyDir run');
+  fs.readdir(path.join(__dirname, './files'), {withFileTypes: true}, (err, files)=>{
+    if (err) {console.error(err)}
+    files.forEach(file=>{
+      fs.copyFile(path.join(__dirname, './files', file.name), path.join(__dirname, './files-copy', file.name), (err)=>{
+        if (err) {
+          console.error(err);
+          return;
+        }
+      })
     })
   })
-})
+}
+
